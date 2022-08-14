c9c7b704fa07f8a09be7a45fb0b5560e71fba3fc
Enes Taşyürek <tasyurekenes22@gmail.com>
2022-08-15 01:23:16 +0300

    Class

--- src/JAVA/Ring.java
+++ src/JAVA/Ring.java
@@ -2,8 +2,28 @@ package JAVA;
 
 public class Ring {
     public static void main(String[] args) {
-        System.out.println("Fight");
+        Fight fight = new Fight();
+        System.out.println(fight);
+
+        Fight fight1 = new Fight();
+        fight1.name = "Sefa";
+        fight1.age = 20;
+        System.out.println(fight1);
     }
 
     
+}
+
+class Fight {
+
+    public String name = "Enes";
+    public int age;
+
+    @Override
+    public String toString() {
+        return "Fight{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
