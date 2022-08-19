25fcbae17ab7a095c314fb74a75665e802664d09
Enes Taşyürek <tasyurekenes22@gmail.com>
2022-08-19 22:07:27 +0300

    setter getter

--- src/JAVA/Ring.java
+++ src/JAVA/Ring.java
@@ -16,6 +16,22 @@ class Fight {
         this.age = age;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     @Override
     public String toString() {
         return "Fight{" +
