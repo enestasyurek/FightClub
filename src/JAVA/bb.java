e6ea601360b5b1e038ec21f3e433024e29df8528
Enes Taşyürek <tasyurekenes22@gmail.com>
2022-08-25 00:45:06 +0300

    inheritance

--- src/JAVA/bb.java
+++ src/JAVA/bb.java
@@ -1,8 +1,11 @@
 package JAVA;
 
 public class bb {
+    public boolean a;
     public static void main(String[] args) {
         aa a1 = new aa();
         System.out.println(a1.name);
+
+
     }
 }
