cdf4f8023e55614f86f5accf0748dfe50beed277
Enes Taşyürek <tasyurekenes22@gmail.com>
2022-12-16 21:10:03 +0300

    reversed

--- src/JAVA/Garbagecollector.java
+++ src/JAVA/Garbagecollector.java
@@ -3,24 +3,6 @@ package JAVA;
 public class Garbagecollector {
 
     public static void main(String[] args) {
-        String str = "Java";
-        str = null;
-        System.out.println();
-        System.out.println(str);
-
-
-        System.out.println("-------");
-
-        Fight fight1 = new Fight("Enes",22);
-        Fight fight2 = new Fight("İsmail",42);
-        //Garbagecollector
-        fight1 = null;
-        fight1 = fight2;
-
-
-
-        System.out.println(fight1);
-        System.out.println(fight2);
     }
 
 
